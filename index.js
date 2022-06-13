ee94ab9aa42a84bad69d3afa62d350cb53ae9326
tmoino <tony.moino@student.moringaschool.com>
2022-06-13 14:06:51 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+/*document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  console.log("tony") 
+  */
+
+
+
+
+document.addEventListener( "DOMContentLoaded", function () {
+    LoadedDom()
+  } );
+  
+// using the function in the kept inside the DOMContentLoaded to replace the text
+  function LoadedDom() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
